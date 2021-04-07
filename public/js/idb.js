cb3421d74145d1a5c63d3c4c3461ec4c7ccb1064
Shanshan Xu <tinaxu84@gmail.com>
2021-04-07 13:33:59 -0400

    add manifest.json

--- public/js/idb.js
+++ public/js/idb.js
@@ -40,7 +40,7 @@ function uploadBudget() {
 
     getAll.onsuccess = function() {
         if (getAll.result.length > 0) {
-            fetch('/api/transaction/bulk', {
+            fetch('/api/transaction', {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
